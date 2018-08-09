2bee7e995dbf1b5cb823f692a913ac472b38580a
Sera Yang <sera.yang@berkeley.edu>
2018-08-09 11:10:20 -0700

    backup

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 import facebook
 import requests
+from urllib.parse import urlparse
 
-access_token = "EAAGdXfsupUEBAI6GSZAWkYeg9JERRPZBqeZBj1WfScNJWtqpwutz5DRh6d3t3K2b483DZAJ5XZBvDkgiENM7FnQukNiA6ZB5t9815uYYwj2Cs15ZC4KDkbb7K4R0Yz72RcxqE8dNMZAVrPbAX0gPvXX4S7n6FFDFSo238DlPP1QWSQKuROngBhwBkceOfgBgZBbcZD"
+access_token = "EAAGdXfsupUEBAGYMvXQmQa98BITUlBXiz7ZCyceG6iHAZAfmYPYhXYfZCExDZBBx62WVMOuq8Wf24P4GYgTJTX5BNYCIanSoheO736kPloCKaZAmIrxV2102UbPJwRJ5naxkYLRVP0Umaj91z28h1YFl7C9OAaooZD"
 graph = facebook.GraphAPI(access_token=access_token, version="2.1")
 
+# Retrieve information about a website or page:
+# https://developers.facebook.com/docs/graph-api/reference/url/
+# Note that URLs need to be properly encoded with the "quote" function
+# of urllib (Python 2) or urllib.parse (Python 3).
+url = urlparse('https://www.facebook.com/groups/149609765847752/?ref=br_rs').geturl()
 
-resp = requests.get('https://todolist.example.com/tasks/')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-for todo_item in resp.json():
-    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+site_info = graph.get_object(id="149609765847752",
+                             fields="og_object")
+print(site_info)
